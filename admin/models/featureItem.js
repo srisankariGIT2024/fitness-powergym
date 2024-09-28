79d3c9a62d0d06f358c67eb716518b9e033b6a29
srisankariGIT <sreelogi24@gmail.com>
2024-09-28 09:42:32 +0530

    FeatureItem data is fetched
    FeatureItem data is fetched

--- admin/models/featureItem.js
+++ admin/models/featureItem.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const featureItemSchema = new mongoose.Schema({
-    itemIcon: String, // This should match the data type in your MongoDB
+    itemIcon: String, 
     itemTitle: String,
     itemDescription: String
 });
--- admin/routes/featureItem.js
+++ admin/routes/featureItem.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const FeatureItem = require('../models/featureItem');
 
-// Get all feature items
 router.get('/', async (req, res) => {
+    console.log('Fetching feature items...'); // Log when the route is hit
     try {
         const featureItems = await FeatureItem.find();
         res.json(featureItems);
     } catch (err) {
+        console.error(err); // Log the error
         res.status(500).json({ message: err.message });
     }
 });
